25b64e92c15c7123be126954a72178262d6cf10a
tindyc <tindyc@bath.edu>
2021-02-20 21:40:33 +0000

    Link contact form to emailjs

---
+++ assets/js/sendemail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "quizijuanna", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.message.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                alert("Thank you for contacting us. We'll respond within 24hours!");
+            },
+            function (error) {
+                console.log("FAILED", error);
+                alert("Something wrong happened!");
+            }
+        );
+    return false;
+}
