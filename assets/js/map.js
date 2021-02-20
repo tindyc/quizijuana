eb7f2671ac1d8c97db8ae9fc40352864ed0ea295
tindyc <tindyc@bath.edu>
2021-02-20 21:36:31 +0000

    Add google map with marker and info window

---
+++ assets/js/map.js
@@ -0,0 +1,31 @@
+function initMap() {
+    // Map options
+    var options = {
+        zoom: 8,
+        center: { lat: 51.5074, lng: 0.1279 },
+    };
+
+    //New Map
+    var map = new
+        google.maps.Map(document.getElementById('map'), options);
+
+    // Add Marker
+    var marker = new google.maps.Marker({
+        position: { lat: 51.5028, lng: 0.0877 },
+        map: map,
+        icon: 'https://img.icons8.com/plasticine/50/000000/marijuana-leaf.png'
+    });
+
+    // Add infoWindow
+    var contentString = '<div id="content"><h1>Quizijuana</h1><p>Quizjuana is ' +
+        'your online guide to learn about marijuana, find marijuana strains and products, and source them from legal,' +
+        'licensed retailers.</p></div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+}
